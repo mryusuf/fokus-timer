9e5c7393e69f0e4f7ceb29be0ed014616992e68d
mryusuf <muhamadyusufindra@gmail.com>
2020-08-20 19:12:21 +0700

    Initial Commit

---
+++ Fokus Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fokus Timer
+//
+//  Created by Indra Permana on 20/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
